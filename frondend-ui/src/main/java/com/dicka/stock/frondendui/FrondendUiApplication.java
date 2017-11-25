723888844df8f2977ba81a6efb996db2782f0479
dickanirwansyah <dickanirwansyah@gmail.com>
2017-11-25 19:30:06 +0700

    user interface

---
+++ frondend-ui/src/main/java/com/dicka/stock/frondendui/FrondendUiApplication.java
@@ -0,0 +1,12 @@
+package com.dicka.stock.frondendui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrondendUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrondendUiApplication.class, args);
+	}
+}
